889305631dd2adb4e67aaa706a7eb9c8c74bbd49
dinny <164994200+dinnyam@users.noreply.github.com>
2024-05-28 05:58:59 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-de588f66-2d34-487b-8082-97694ad4575c/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-e52ccded-d66e-40bb-8a54-5d9519da3a79/)
 
 
 echo "$curl_output" >> Update.tsx
